602187c297f3d1eae261b402e9951b23d6bb58b4
IAS\Daniel.Jackson <daniel.jackson@iris.co.uk>
2024-03-12 14:43:42 +0000

    expression parser

---
+++ Optix.Movies.Application/Filtering/ExpressionParser.cs
@@ -0,0 +1,292 @@
+﻿using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Numerics;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Optix.Movies.Application.Filtering
+{
+    public interface IExpressionParser
+    {
+        List<PropertyInfo> Properties { get; set; }
+        object Value { get; set; }
+        void DefineValue(string filterValues, Type itemType);
+        Expression GetExpression(ParameterExpression parameter, Type itemType, string filterAndValue);
+        (string[], PropertyInfo) DefineOperation(string filterValues, Type itemType);
+
+    }
+    public class ExpressionParser : IExpressionParser
+    {
+
+        public List<PropertyInfo> Properties { get; set; }
+        public object Value { get; set; }
+        public Operator Condition { get; set; }
+
+        //for testing
+        public ExpressionParser(string filterValues, Type itemType)
+        {
+            DefineValue(filterValues, itemType);
+        }
+
+        //for testing
+        public ExpressionParser()
+        {
+
+        }
+
+        // so we can use DI
+        public void DefineValue(string filterValues, Type itemType)
+        {
+            Properties = new List<PropertyInfo>();
+            Value = new object();
+            var (values, _) = DefineOperation(filterValues, itemType);
+            if (values.Any())
+            {
+                Value = ParseValue(values[1]);
+            }
+        }
+
+        public Expression GetExpression(ParameterExpression parameter, Type itemType, string filterAndValue)
+        {
+            DefineValue(filterAndValue, itemType);
+            if (Value != null)
+            {
+                var expression = GetExpression(parameter);
+                return expression;
+            }
+            return null;
+        }
+
+        private Expression GetExpression(ParameterExpression parameter)
+        {
+            var constantExpression = Expression.Constant(Value);
+            Expression returnExpression;
+
+            if (!Properties.Any())
+            {
+                return null;
+            }
+
+            //making constant nullable
+            if (Nullable.GetUnderlyingType(Properties[Properties.Count - 1].PropertyType) != null)
+            {
+                var type = typeof(Nullable<>).MakeGenericType(Nullable.GetUnderlyingType(Properties[Properties.Count - 1].PropertyType));
+                constantExpression = Expression.Constant(Value, type);
+            }
+
+            Expression body = parameter;
+            foreach (var member in Properties)
+            {
+                body = Expression.Property(body, member);
+            }
+
+            switch (Condition)
+            {
+                default:
+                case Operator.Equals:
+                    {
+                        returnExpression = Expression.Equal(body, constantExpression);
+                        break;
+                    }
+                case Operator.Contains:
+                    {
+                        constantExpression = Expression.Constant(Value.ToString().ToLower());
+
+                        MethodInfo toLowerMethod = typeof(string).GetMethod("ToLowerInvariant");
+
+                        var expression1 = Expression.Call(body, toLowerMethod);
+
+                        MethodInfo method = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+
+                        returnExpression = Expression.Call(expression1, method, constantExpression);
+
+                        break;
+
+                    }
+                case Operator.ContainsCaseSensitive:
+                    {
+                        MethodInfo method = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+                        returnExpression = Expression.Call(body, method, constantExpression);
+
+                        break;
+                    }
+                case Operator.GreaterThan:
+                    {
+                        returnExpression = Expression.GreaterThan(body, constantExpression);
+
+                        break;
+                    }
+                case Operator.LessThan:
+                    {
+                        returnExpression = Expression.LessThan(body, constantExpression);
+                        break;
+                    }
+                case Operator.GreaterOrEqual:
+                    {
+                        returnExpression = Expression.GreaterThanOrEqual(body, constantExpression);
+                        break;
+                    }
+                case Operator.LessOrEqual:
+                    {
+                        returnExpression = Expression.LessThanOrEqual(body, constantExpression);
+                        break;
+                    }
+                case Operator.NotEquals:
+                    {
+                        returnExpression = Expression.NotEqual(body, constantExpression);
+                        break;
+                    }
+            }
+
+            return returnExpression;
+        }
+
+
+        public (string[], PropertyInfo) DefineOperation(string filterValues, Type itemType)
+        {
+            string[] values = null;
+
+            if (filterValues.Contains('='))
+            {
+                values = filterValues.Split('=');
+                Condition = Operator.Equals;
+            }
+
+            if (filterValues.Contains('%'))
+            {
+                if (filterValues.Contains("%%"))
+                {
+                    Condition = Operator.ContainsCaseSensitive;
+                    values = Regex.Split(filterValues, "%%");
+                }
+                else
+                {
+                    Condition = Operator.Contains;
+                    values = filterValues.Split('%');
+                }
+            }
+
+            if (filterValues.Contains('>'))
+            {
+                values = filterValues.Split('>');
+                Condition = Operator.GreaterThan;
+            }
+
+            if (filterValues.Contains('<'))
+            {
+                values = filterValues.Split('<');
+                Condition = Operator.LessThan;
+            }
+
+            if (filterValues.Contains(">="))
+            {
+                values = Regex.Split(filterValues, ">=");
+                Condition = Operator.GreaterOrEqual;
+            }
+
+            if (filterValues.Contains("<="))
+            {
+                values = Regex.Split(filterValues, "<=");
+                Condition = Operator.LessOrEqual;
+            }
+
+            if (filterValues.Contains("!="))
+            {
+                values = Regex.Split(filterValues, "!=");
+                Condition = Operator.NotEquals;
+            }
+
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(filterValues));
+            }
+
+            var property = itemType.GetProperty(values[0], BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.Instance);
+
+            if (property != null)
+            {
+                Properties?.Add(property);
+            }
+
+            return (values, property);
+        }
+
+        public object ParseValue(string value)
+        {
+            object parsedValue = null;
+
+            foreach (var property in Properties.Select(property => property.PropertyType))
+            {
+                if (property.IsClass && property.Name.ToLower() != "string" && property.Name.ToLower() != "datetime")
+                {
+                    continue;
+                }
+                //Verifying if is nullable
+                if (Nullable.GetUnderlyingType(property) != null)
+                {
+                    var underlyingType = Nullable.GetUnderlyingType(property);
+                    var type = typeof(Nullable<>).MakeGenericType(underlyingType);
+
+                    var newValue = underlyingType.IsEnum ?
+                            Enum.Parse(underlyingType, value) :
+                            ChangeType(value, underlyingType);
+
+                    var nullableObject = Activator.CreateInstance(type, newValue);
+
+                    parsedValue = nullableObject;
+                }
+                else
+                {
+                    parsedValue = ChangeType(value, property);
+                }
+            }
+
+            return parsedValue;
+        }
+
+        private static object ChangeType(string value, Type type)
+        {
+            if (type.IsEnum)
+            {
+                return Convert.ChangeType(Enum.Parse(type, value), type);
+            }
+
+            if (type == typeof(Guid))
+            {
+                return Guid.Parse(value);
+            }
+
+            if (type == typeof(BigInteger))
+            {
+                return BigInteger.Parse(value);
+            }
+
+            if (type == typeof(int))
+            {
+                return int.Parse(value);
+            }
+
+            if (type == typeof(DateTime))
+            {
+                return DateTime.Parse(value);
+            }
+
+            var converter = TypeDescriptor.GetConverter(type);
+
+            return converter.ConvertFrom(value);
+        }
+    }
+
+    public enum Operator
+    {
+        Equals = 1,
+        Contains = 2,
+        ContainsCaseSensitive = 3,
+        GreaterThan = 4,
+        LessThan = 5,
+        GreaterOrEqual = 6,
+        LessOrEqual = 7,
+        NotEquals = 8
+
+    }
+}
---
+++ Optix.Movies.Application/Filtering/QueryFilter.cs
@@ -0,0 +1,219 @@
+﻿using Optix.Movies.Infrastructure.Models;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Optix.Movies.Application.Filtering
+{
+    public interface IQueryFilter<T>
+    {
+        int PageSize { get; set; }
+        int Page { get; set; }
+        SortDirection SortDirection { get; set; }
+        string OrderBy { get; set; }
+
+        Expression<Func<T, bool>> Filter { get; set; }
+        Expression<Func<T, object>> OrderExpression { get; set; }
+        Expression<Func<T, bool>> NotNullExpression { get; set; }
+
+        void Create(string queryString);
+    }
+    public class QueryFilter<T> : IQueryFilter<T> where T : class
+    {
+        public int PageSize { get; set; } = 1000;
+        public int Page { get; set; } = 1;
+        public SortDirection SortDirection { get; set; }
+        public string OrderBy { get; set; }
+
+        public Expression<Func<T, bool>> Filter { get; set; }
+        public Expression<Func<T, object>> OrderExpression { get; set; }
+        public Expression<Func<T, bool>> NotNullExpression { get; set; }
+
+        private readonly IExpressionParser _expressionParser;
+
+        public QueryFilter(IExpressionParser expressionParser)
+        {
+            _expressionParser = expressionParser ?? throw new ArgumentNullException(nameof(expressionParser));
+        }
+
+        public void Create(string queryString)
+        {
+            // TODO: add validation
+
+            Type itemType = typeof(T);
+            var parameter = Expression.Parameter(itemType, "x");
+            var filterAndValues = queryString.Split('&').ToList();
+            ExtractPagination(filterAndValues);
+            ExtractOrder(filterAndValues, itemType);
+            ExtractFilters(filterAndValues, parameter, itemType);
+        }
+
+        private void ExtractPagination(List<string> filter)
+        {
+            var page = filter.Find(x => x.Contains("page", StringComparison.InvariantCultureIgnoreCase));
+            var pageSize = filter.Find(x => x.Contains("pagesize", StringComparison.InvariantCultureIgnoreCase));
+
+            if (!string.IsNullOrWhiteSpace(page))
+            {
+                Page = int.Parse(page.Remove(0, 5));
+            }
+
+            if (!string.IsNullOrWhiteSpace(pageSize))
+            {
+                PageSize = int.Parse(pageSize.Remove(0, 9));
+            }
+        }
+
+        private void ExtractOrder(List<string> filter, Type itemType)
+        {
+            var order = filter.Find(x => x.Contains("order", StringComparison.InvariantCultureIgnoreCase));
+            var orderByProperty = filter.Find(x => x.Contains("orderBy", StringComparison.InvariantCultureIgnoreCase));
+
+            if (!string.IsNullOrWhiteSpace(order))
+            {
+                var orderItems = order.Split('=');
+                if (orderItems.Length == 2)
+                {
+                    SortDirection = orderItems[1].Equals("Asc", StringComparison.InvariantCultureIgnoreCase) ? SortDirection.Ascending : SortDirection.Descending;
+                }
+                else
+                {
+                    SortDirection = SortDirection.Descending;
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(orderByProperty))
+            {
+                var orderProperties = orderByProperty.Split('=');
+                if (orderProperties.Length == 2)
+                {
+                    OrderBy = orderProperties[1];
+                }
+                else
+                {
+                    OrderBy = "Name";
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(order) && string.IsNullOrWhiteSpace(orderByProperty))
+            {
+                OrderBy = "Name";
+            }
+
+            if (OrderBy != null)
+            {
+                PropertyInfo property = itemType.GetProperty(OrderBy, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+                var parameter = Expression.Parameter(itemType, "p");
+                var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+                OrderExpression = (Expression<Func<T, object>>)Expression.Lambda(Expression.Convert(propertyAccess, typeof(Object)).Reduce(), parameter);
+            }
+        }
+
+        private void ExtractFilters(List<string> filter, ParameterExpression parameter, Type itemType)
+        {
+            var excluded = new List<string>() { "order", "page", "pagesize", "orderBy" };
+            var filterAndValues = filter.Where(x =>
+                        !excluded.Exists(e => x.Contains(e, StringComparison.InvariantCultureIgnoreCase))
+                        ).ToList();
+
+            LambdaExpression finalExpression = null;
+
+            AddNotNullExpressions(filterAndValues, parameter, itemType);
+            var currentExpression = GetAndExpression(filterAndValues, parameter, itemType);
+            var currentOrExpression = GetOrExpression(filterAndValues, parameter, itemType);
+
+            if (currentOrExpression != null && currentExpression != null)
+            {
+                currentExpression = Expression.And(currentExpression, currentOrExpression);
+            }
+
+            if (currentOrExpression != null && currentExpression == null)
+            {
+                currentExpression = currentOrExpression;
+            }
+
+            if (currentExpression != null)
+            {
+                finalExpression = Expression.Lambda<Func<T, bool>>(currentExpression, parameter);
+
+                Filter = (Expression<Func<T, bool>>)finalExpression;
+            }
+        }
+
+        private Expression GetAndExpression(List<string> filterAndValues, ParameterExpression parameter, Type itemType)
+        {
+            Expression currentExpression = null;
+            foreach (var fv in filterAndValues.Where(x => !x.Contains("||")))
+            {
+
+
+                Expression expression = _expressionParser.GetExpression(parameter, itemType, fv);
+
+                if (currentExpression == null)
+                {
+                    currentExpression = expression;
+                }
+                else
+                {
+                    currentExpression = Expression.And(currentExpression, expression);
+                }
+            }
+            return currentExpression;
+        }
+
+        private Expression GetOrExpression(List<string> filterAndValues, ParameterExpression parameter, Type itemType)
+        {
+            Expression currentOrExpression = null;
+            foreach (var fvo in filterAndValues.Where(x => x.Contains("||")))
+            {
+
+                var values = fvo.Split("||");
+
+                foreach (var v in values)
+                {
+                    Expression orExpression = _expressionParser.GetExpression(parameter, itemType, v);
+
+                    if (currentOrExpression == null)
+                    {
+                        currentOrExpression = orExpression;
+                    }
+                    else
+                    {
+                        currentOrExpression = Expression.Or(currentOrExpression, orExpression);
+                    }
+                }
+            }
+            return currentOrExpression;
+        }
+
+        private void AddNotNullExpressions(List<string> filterAndValues, ParameterExpression parameter, Type itemType)
+        {
+            Expression currentExpression = null;
+            foreach (var fv in filterAndValues)
+            {
+                if (!fv.Contains('%'))
+                {
+                    continue;
+                }
+
+                var (_, property) = _expressionParser.DefineOperation(fv, itemType);
+                var notNullExpression = Expression.NotEqual(Expression.Property(parameter, property), Expression.Constant(null));
+                if (currentExpression == null)
+                {
+                    currentExpression = notNullExpression;
+                }
+                else
+                {
+                    currentExpression = Expression.And(currentExpression, notNullExpression);
+                }
+
+
+            }
+            if (currentExpression != null)
+            {
+                var finalExpression = Expression.Lambda<Func<T, bool>>(currentExpression, parameter);
+
+                NotNullExpression = finalExpression;
+            }
+        }
+    }
+}
